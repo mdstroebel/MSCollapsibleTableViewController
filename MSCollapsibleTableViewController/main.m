1e564b7d9a80c333e5716b68c5ab9da7d663e206
MD Stroebel <mdstroebel@gmail.com>
2013-10-12 17:34:49 +0200

    Initial Commit

---
+++ MSCollapsibleTableViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MSCollapsibleTableViewController
+//
+//  Created by Marc Stroebel on 2013/10/12.
+//  Copyright (c) 2013 StroebelSoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MSAppDelegate class]));
+    }
+}
